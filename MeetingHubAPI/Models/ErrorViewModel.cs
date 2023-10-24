af28f7d7e4c904c7e8330aa3e6204a4e99b586e2
Resul KIZMAZ <algast12@gmail.com>
2023-10-24 13:05:05 +0300

    Created API

---
+++ MeetingHubAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeetingHubAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
